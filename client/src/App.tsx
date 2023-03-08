05f26c4496a77f185516aec718eea9dbc420417f
tanerijun <tanerivince@gmail.com>
2023-03-08 22:59:57 +0800

    Basic TODO skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,65 @@
-import { useEffect, useState } from "react";
+import styled from "@emotion/styled";
+
+const AppContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
+`;
+
+const AppHeader = styled.div`
+	display: flex;
+	align-items: center;
+	height: 60px;
+	width: 100%;
+	padding-left: 20px;
+	box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+`;
+
+const TodoContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	align-items: center;
+	justify-content: center;
+	flex: 1;
+`;
+
+const dummyTodos = [
+	{
+		id: 1,
+		text: "Learn React",
+	},
+	{
+		id: 2,
+		text: "Learn TypeScript",
+	},
+	{
+		id: 3,
+		text: "Learn GraphQL",
+	},
+];
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		<div className="App">
-			<EmotionTest />
-			<EmotionTestTwo />
-			<ServerData />
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-			</div>
-		</div>
+		<AppContainer>
+			<AppHeader>
+				<h1>lavender-snake : TODO LIST</h1>
+			</AppHeader>
+			<TodoContainer>
+				<input placeholder="What needs to be done?" />
+				<ul>
+					{dummyTodos.map((todo) => (
+						<li key={todo.id}>{todo.text}</li>
+					))}
+				</ul>
+				<div>
+					TODO COUNT: <span>{dummyTodos.length}</span>
+				</div>
+			</TodoContainer>
+		</AppContainer>
 	);
-}
-
-function EmotionTest() {
-	return <div css={{ backgroundColor: "red" }}>Hello, Emotion!</div>;
-}
-
-function EmotionTestTwo() {
-	return <div css={{ backgroundColor: "blue" }}>Hello, by MagicBeam</div>;
-}
-
-function ServerData() {
-	const [serverCount, setServerCount] = useState("Loading...");
-	useEffect(() => {
-		fetch("http://localhost:3000/count")
-			.then((res) => res.json())
-			.then((resp) => {
-				console.log(resp);
-				setServerCount(`Server count is: ${resp.count}`);
-			})
-			.catch((err) => {
-				console.log(err);
-			});
-	}, []);
-
-	return <div>{serverCount}</div>;
 }
 
 export default App;
