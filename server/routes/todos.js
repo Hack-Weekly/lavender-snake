b024831b9cf0a5e34f465be481e4cab351fbec10
tanerijun <tanerivince@gmail.com>
2023-03-08 23:33:32 +0800

    Add nodemon for hot reloading

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -2,7 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 // Store in memory for now
-const todos = [];
+const todos = [
+	{
+		id: 1,
+		text: "Learn React",
+		completed: false,
+	},
+	{
+		id: 2,
+		text: "Learn TypeScript",
+		completed: true,
+	},
+	{
+		id: 3,
+		text: "Learn GraphQL",
+		completed: false,
+	},
+];
 
 router.get("/", function (req, res, next) {
 	// TODO - return all todos
