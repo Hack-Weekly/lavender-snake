fc167bf9ad92a267ceaa4df97396dcd14d2aa2e1
tanerijun <tanerivince@gmail.com>
2023-03-08 13:10:05 +0800

    Try making some changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,61 +1,63 @@
-import { jsx } from '@emotion/react'
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { jsx } from "@emotion/react";
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <EmotionTest />
-      <ServerData />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div className="App">
+			<EmotionTest />
+			<EmotionTestTwo />
+			<ServerData />
+			<div>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src="/vite.svg" className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://reactjs.org" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Vite + React</h1>
+			<div className="card">
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</div>
+	);
 }
 
 function EmotionTest() {
-  return (
-    <div css={{backgroundColor: 'red'}}>Hello, Emotion!</div>
-  )
+	return <div css={{ backgroundColor: "red" }}>Hello, Emotion!</div>;
+}
+
+function EmotionTestTwo() {
+	return <div css={{ backgroundColor: "blue" }}>Hello, by MagicBeam</div>;
 }
 
 function ServerData() {
-  const [serverCount, setServerCount] = useState('Loading...')
-  useEffect (() => {
-    fetch('http://localhost:3000/count')
-      .then((res) => res.json())
-      .then((resp) => {
-        console.log(resp)
-        setServerCount(`Server count is: ${resp.count}`)
-      })
-      .catch((err) => {
-        console.log(err)
-      }
-    )
-  }, [])
+	const [serverCount, setServerCount] = useState("Loading...");
+	useEffect(() => {
+		fetch("http://localhost:3000/count")
+			.then((res) => res.json())
+			.then((resp) => {
+				console.log(resp);
+				setServerCount(`Server count is: ${resp.count}`);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, []);
 
-  return (<div>{serverCount}</div>)
+	return <div>{serverCount}</div>;
 }
 
-export default App
+export default App;
