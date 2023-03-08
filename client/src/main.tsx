58f642643ac3dc40000c09d8e6eee78b7ccbba63
tanerijun <tanerivince@gmail.com>
2023-03-08 22:07:43 +0800

    Normalize css

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Global, css } from "@emotion/react";
+import normalize from "normalize.css/normalize.css?inline";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
+		<Global
+			styles={css`
+				${normalize}
+			`}
+		/>
 		<App />
 	</React.StrictMode>
 );
