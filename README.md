f87fc040c2696a8117507070e45a35ab9e69c345
Ryan Rolnicki <git@rolnicki.net>
2023-03-08 09:34:54 +0900

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # lavender-snake
-Repo for Lavender Snake team
+Repo for Lavender Snake team!
+
+This repo is organized into 2 components (for now): 
+  client - where the web client (react) code goes
+  server - where our web server (express) code goes
---
+++ client/src/App.tsx
@@ -0,0 +1,56 @@
+import { jsx } from '@emotion/react'
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <EmotionTest />
+      <ServerData />
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+function EmotionTest() {
+  return (
+    <div css={{backgroundColor: 'red'}}>Hello, Emotion!</div>
+  )
+}
+
+function ServerData() {
+  const [serverCount, setServerCount] = useState('Loading...')
+  useEffect (() => {
+    fetch('http://localhost:3000').then((res) => {
+      console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }, [])
+
+  return (<div>{serverCount}</div>)
+}
+
+export default App
