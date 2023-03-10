4829d527e415568f964426a9b9107830cafae149
ejsadiarin <ejsadiarin@gmail.com>
2023-03-10 13:56:44 +0800

    added delete transition

--- client/src/components/TodoEntry.tsx
+++ client/src/components/TodoEntry.tsx
@@ -14,87 +14,96 @@ interface TodoItemProps {
 }
 
 const TodoItemContainer = styled.div`
-	min-width: 15rem;
-	max-width: 30rem;
-	display: flex;
-	flex-direction: row;
-	align-items: center;
-	margin: 0.5rem 0;
-	font-size: 1.4rem;
+  min-width: 15rem;
+  max-width: 30rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 0.5rem 0;
+  font-size: 1.4rem;
+	transition: opacity 0.6s ease-out, height 0.4s ease-out;
 `;
 
 const TodoItem = styled.div<TodoItemProps>`
-	margin-left: 0.5rem;
-	color: ${(props) => (props.isCompleted && "#7A7E88")};
-	text-decoration: ${(props) => (props.isCompleted ? "line-through" : "none")};
+  margin-left: 0.5rem;
+  color: ${(props) => (props.isCompleted && "#7A7E88")};
+  text-decoration: ${(props) => (props.isCompleted ? "line-through" : "none")};
 `;
 
 const DeleteButton = styled.div`
-	margin-left: auto;
-	cursor: pointer;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	&:hover svg{
-		stroke: #61A4D9;
-	}
+  margin-left: auto;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  &:hover svg {
+    stroke: #61A4D9;
+  }
 `;
 
 const TodoStyleNoFade = css`
-	opacity: 1;
-	transition: opacity 0.6s ease-out, height 0.4s ease-out;
-	height: auto;
+  opacity: 1;
+  height: 3.5rem;
 `;
 
 const TodoStyleFadeTriggered = css`
-	margin: 0px;
-	padding: 0px;
-	height: 0px;
-	opacity: 0;
+  opacity: 0;
+  height: 0px;
+  padding: 0px;
+  margin: 0px;
 `;
 
-
 function TodoEntry({ todo }: any) {
-	const [, setTodos] = useTodos();
-	const [isAnimating, setIsAnimating] = useState(false);
+  const [, setTodos] = useTodos();
+  const [isAnimating, setIsAnimating] = useState(false);
 
-	const removeTodo = async () => {
-		const todos = await apiClient.deleteTodo(todo);
-		setTodos(todos);
-	};
+  const removeTodo = async () => {
+    const todos = await apiClient.deleteTodo(todo);
+    setTodos(todos);
+  };
 
-	const handleCompleted = async () => {
-		const todos = await apiClient.updateTodo({
-			...todo,
-			completed: !todo.completed,
-		});
-		setTodos(todos);
-	};
+  const handleCompleted = async () => {
+    const todos = await apiClient.updateTodo({
+      ...todo,
+      completed: !todo.completed,
+    });
+    setTodos(todos);
+  };
 
-	const handleDeleted = () => {
-		setIsAnimating(true);
-
-		setTimeout(() => {
-			setIsAnimating(false);
-			removeTodo();
-		}, 800);
+  const handleDeleted = () => {
+    setIsAnimating(true);
 	}
 
-	return (
-		<TodoItemContainer key={todo.id} css={isAnimating ? [TodoStyleNoFade, TodoStyleFadeTriggered] : TodoStyleNoFade} >
-			<IconContext.Provider value={{ color: "#61A4D9" }}>
-				{(todo.completed) ? <AiFillCheckCircle/> : <BsCircle/>}
-			</IconContext.Provider>
-				<TodoItem isCompleted={todo.completed} onClick={() => handleCompleted()}>
-					{todo.text}
-				</TodoItem>
-			<IconContext.Provider value={{ color: "hsl(198, 1%, 29%)" }}>
-				<DeleteButton>
-					<IoTrashOutline onClick={() => handleDeleted()}/>
-				</DeleteButton>
-			</IconContext.Provider>
-		</TodoItemContainer>
-	);
+	const handleTransitionEnd = async () => {
+		if (isAnimating) {
+			setIsAnimating(false);
+			await removeTodo();
+		}
+	};
+	
+
+  return (
+    <TodoItemContainer
+			key={todo.id}
+      css={isAnimating ? TodoStyleFadeTriggered : TodoStyleNoFade}
+			onTransitionEnd={handleTransitionEnd}
+    >
+      <IconContext.Provider value={{ color: "#61A4D9" }}>
+        {todo.completed ? <AiFillCheckCircle /> : <BsCircle />}
+      </IconContext.Provider>
+      <TodoItem
+        isCompleted={todo.completed}
+        onClick={() => handleCompleted()}
+      >
+        {todo.text}
+      </TodoItem>
+      <IconContext.Provider value={{ color: "hsl(198, 1%, 29%)" }}>
+        <DeleteButton>
+          <IoTrashOutline onClick={() => handleDeleted()} />
+        </DeleteButton>
+      </IconContext.Provider>
+    </TodoItemContainer>
+  );
 }
 
 export default TodoEntry;
