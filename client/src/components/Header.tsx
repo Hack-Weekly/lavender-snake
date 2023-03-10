54a776fdd3d6a838c7fe81addeb74c4159f3ba06
tanerijun <tanerivince@gmail.com>
2023-03-10 22:30:34 +0800

    Add logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,13 +3,16 @@ import styled from "@emotion/styled";
 const HeaderContainer = styled.div`
 	display: flex;
 	align-items: center;
+	justify-content: space-between;
 	height: 3.5rem;
 	padding-left: 1.2rem;
+	padding-right: 1.2rem;
 `;
 
 export default function Header() {
 	return (
 		<HeaderContainer>
+			<img src="/lavender-snake.png" alt="logo" width="60" height="60" />
 			<h1>To Do List by Lavender Snake</h1>
 		</HeaderContainer>
 	);
