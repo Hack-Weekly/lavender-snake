362ef3c41fbe2fea1e369c3689ecc7649997571c
tanerijun <tanerivince@gmail.com>
2023-03-18 21:08:13 +0800

    Implement signup endpoint

---
+++ server/src/user/data.ts
@@ -0,0 +1,61 @@
+import { generateId } from '@/utils/generateId'
+import type { User } from '@shared/chatTypes'
+
+interface UserAccount {
+  email: string
+  password: string
+  user: User
+}
+
+const userAccounts: UserAccount[] = [
+  {
+    email: 'bob@dummy.com',
+    password: 'bob123',
+    user: {
+      id: 'bobid',
+      name: 'Bob',
+      picture: 'https://robohash.org/Bob',
+    },
+  },
+  {
+    email: 'frank@dummy.com',
+    password: 'frank123',
+    user: {
+      id: 'frankid',
+      name: 'Frank',
+      picture: 'https://robohash.org/Frank',
+    },
+  },
+  {
+    email: 'tim@dummy.com',
+    password: 'tim123',
+    user: {
+      id: 'timid',
+      name: 'Tim',
+      picture: 'https://robohash.org/Tim',
+    },
+  },
+]
+
+export function addUser(
+  email: string,
+  password: string,
+  username: string
+): User {
+  const userAccount = {
+    email,
+    password,
+    user: {
+      id: generateId(),
+      name: username,
+      picture: `https://robohash.org/${username}`,
+    },
+  }
+
+  userAccounts.push(userAccount)
+  return userAccount.user
+}
+
+export function getUserByEmail(email: string) {
+  return userAccounts.find((user) => user.email === email)
+}
--- server/src/user/routes.ts
+++ server/src/user/routes.ts
@@ -1,13 +1,43 @@
-export default function authHandler(server, options, done) {
+import { isValidEmail, isValidPassword } from '@/utils/validators'
+import { addUser, getUserByEmail } from './data'
+
+export default function userHandler(server, options, done) {
   server.get('/', { onRequest: [server.authenticate] }, async (req, reply) => {
     reply.send({ message: 'hello from auth' })
   })
 
   server.post('/signup', async (req, reply) => {
-    // get email and password from request body
-    // check if email is taken
-    const token = server.jwt.sign({ username: 'bob' })
-    reply.send({ token })
+    const { email, password, username } = req.body
+
+    if (!email || !password || !username) {
+      reply.code(400).send({ message: 'missing fields' })
+      return
+    }
+
+    if (!isValidEmail(email.toLowerCase())) {
+      reply.code(400).send({ message: 'invalid email' })
+      return
+    }
+
+    if (!isValidPassword(password)) {
+      reply
+        .code(400)
+        .send({ message: 'password has to be at least 4 characters' })
+      return
+    }
+
+    const user = getUserByEmail(email.toLowerCase())
+    if (user) {
+      reply.code(400).send({
+        message: `user with the email ${email.toLowerCase()} already exist`,
+      })
+      return
+    }
+
+    const userData = addUser(email.toLowerCase(), password, username)
+
+    const token = server.jwt.sign({ id: userData.id })
+    reply.send({ ...userData, jwt: token })
   })
 
   done()
