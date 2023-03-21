f46e1eaa3b3dc2a371bc66819b72d129b8acc264
Ryan Rolnicki <git@rolnicki.net>
2023-03-22 07:38:30 +0900

    Add socket io

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,8 @@ import userHandler from './user/routes.js'
 import chatHandler from './chat/routes.js'
 import fastifyCors from '@fastify/cors'
 import jwt from './plugins/jwt.js'
+import socket from 'socket.io'
+import fastifyIO from 'fastify-socket.io'
 
 export function createServer() {
   const server = fastify()
@@ -11,6 +13,13 @@ export function createServer() {
   server.register(jwt)
   server.register(userHandler, { prefix: '/user' })
   server.register(chatHandler, { prefix: '/chat' })
+  server.register(fastifyIO as any)
+  server.ready().then(() => {
+    // we need to wait for the server to be ready, else `server.io` is undefined
+    server.io.on('connection', (socket) => {
+      console.log(socket.id)
+    })
+  })
 
   server.setErrorHandler((error, req, res) => {
     req.log.error(error.toString())
