6d529309f301d6ac72c24f797842d7bc941edd4c
Ryan Rolnicki <git@rolnicki.net>
2023-03-24 15:42:21 +0900

    Unneeded config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -15,29 +15,4 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:3000",
-				changeOrigin: true,
-				secure: true,
-				ws: true,
-				configure: (proxy, _options) => {
-					proxy.on("error", (err, _req, _res) => {
-						console.log("proxy error", err);
-					});
-					proxy.on("proxyReq", (proxyReq, req, _res) => {
-						console.log("Sending Request to the Target:", req.method, req.url);
-					});
-					proxy.on("proxyRes", (proxyRes, req, _res) => {
-						console.log(
-							"Received Response from the Target:",
-							proxyRes.statusCode,
-							req.url
-						);
-					});
-				},
-			},
-		},
-	},
 });
