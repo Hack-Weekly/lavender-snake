d298159812ee1ef9188a62bc12a001be180a6eaf
ejsadiarin <ejsadiarin@gmail.com>
2023-03-26 01:25:34 +0800

    fix

--- client/src/chat/components/HomeScreen.tsx
+++ client/src/chat/components/HomeScreen.tsx
@@ -7,14 +7,6 @@ import { MdAccountCircle, MdAddCircle } from "react-icons/md";
 import { chatColors } from "@/chatColors";
 import { brandGradient } from "@/branding";
 import dummyProfilePic1 from "../../chatImages/3.jpg";
-import * as Dialog from '@radix-ui/react-dialog';
-import {
-	DialogContent,
-	DialogDescription,
-	DialogOverlay,
-	DialogTitle,
-	IconButton,
-} from "@/components/Dialog";
 import { DateTime } from "luxon";
 
 const homeScreenCSS = {
@@ -226,32 +218,9 @@ function ChatList() {
 }
 
 function NewChatButton() {
-	
 	return (
 		<div css={homeScreenCSS.newChatButton}>
-			<Dialog.Root>
-				<Dialog.Trigger asChild>
-					<MdAddCircle />
-				</Dialog.Trigger>
-				<Dialog.Portal>
-				<DialogOverlay />
-					<DialogContent>
-						<DialogTitle>Add new chat:</DialogTitle>
-						<DialogDescription>Test dialog... maybe search bar here filtering names?</DialogDescription>
-						<button type="submit">Create</button>
-						<ul>
-							<li>User Placeholder - TODO: styles, allow y-scroll </li>
-							<li>User Placeholder - when clicked, have an active hover state?</li>
-							<li>User Placeholder</li>
-							<li>User Placeholder</li>
-							<li>User Placeholder</li>
-						</ul>
-						<Dialog.Close asChild>
-						<IconButton aria-label="Close">X</IconButton>
-						</Dialog.Close>
-					</DialogContent>
-				</Dialog.Portal>
-			</Dialog.Root>
+			<MdAddCircle />
 		</div>
 	);
 }
