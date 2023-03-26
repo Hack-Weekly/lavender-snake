69d5e5a2eb992f26c3ce4a72305549476aeb74f0
Ryan Rolnicki <git@rolnicki.net>
2023-03-26 15:26:31 +0900

    build fix

--- server/src/chat/routes.ts
+++ server/src/chat/routes.ts
@@ -14,7 +14,7 @@ import {
   chatStorageClient,
   threadStorageClient,
   usersStorageClient,
-} from '@/storageClients.js'
+} from '../storageClients.js'
 
 function randChoice<T>(arr: Array<T>): T {
   return arr[Math.floor(Math.random() * arr.length)]
